14ac66eae8d8043841c068cdb4460236d60b5a81
hanaa eyadah <hanaa.eyadah@outlook.com>
2021-03-07 21:01:25 +0300

    Almost Done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const products = require("./products");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.listen(8000, () => {
+    app.listen("The application is running on localhost:8000");
+  });
---
+++ products.js
@@ -0,0 +1,31 @@
+
+
+const products = [
+  {
+    id: 1,
+    name: "Ps5 Console",
+    slug: "Ps5 Console",
+    price: 166,
+    // image: img1,
+    description: "ps5 Console Standerd Version  ",
+  },
+
+  {
+    id: 2,
+    name: "Gameing PC",
+    slug: "Gaming PC",
+    price: 500,
+    // image: img2,
+    description: " Intel-i7 NAVIDIA GTX-1660 1T SSD ",
+  },
+  {
+    id: 3,
+    name: "Gaming Chair",
+    slug: "Gaming Chair",
+    price: 40,
+    // image: img3,
+    description: " LED Gaming Chair with Footrest ",
+  },
+];
+
+module.exports = products;
