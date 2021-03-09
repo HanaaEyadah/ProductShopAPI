4435cb863a1f931b15ff8f49397141b366e20af6
hanaa eyadah <hanaa.eyadah@outlook.com>
2021-03-09 19:25:47 +0300

    Edit express.jason

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 let products = require("./products");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.get("/products", (req, res) => {
