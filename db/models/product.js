fec146db620c4748fe5d76a705b580f56f8ae6e7
hanaa eyadah <hanaa.eyadah@outlook.com>
2021-03-17 00:59:26 +0300

    Done

--- db/models/product.js
+++ db/models/product.js
@@ -1,17 +1,34 @@
+
 module.exports = (sequelize, DataTypes) => {
+    const SequelizeSlugify = require("sequelize-slugify");
     const Product = sequelize.define("Product", {
-      name: {
-        type: DataTypes.STRING,
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        slug: {
+          type: DataTypes.STRING,
+          unique: true
+        },
+        description: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+    defaultValue: 5,
+    validate: {
+      min: 1
+        },
+        image: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
       },
-      description: {
-        type: DataTypes.STRING,
-      },
-      price: {
-        type: DataTypes.INTEGER,
-      },
-      image: {
-        type: DataTypes.STRING,
-      },
-    });
+    })
+    
+  SequelizeSlugify.slugifyModel(Product, {
+    source: ["name"]
+  });
     return Product;
-  };
+      };
